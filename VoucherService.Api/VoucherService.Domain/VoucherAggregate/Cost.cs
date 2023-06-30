1788c5bfab85763a3c435f35f6e32fe1b41026c4
Matthew LaMeyer <7293501+mlameyer@users.noreply.github.com>
2023-06-29 22:04:50 -0500

    Update Cost.cs
    adding cost structure

--- VoucherService.Api/VoucherService.Domain/VoucherAggregate/Cost.cs
+++ VoucherService.Api/VoucherService.Domain/VoucherAggregate/Cost.cs
@@ -1,8 +1,30 @@
-﻿using VoucherService.Domain.SeedWork;
+﻿using System;
+using VoucherService.Domain.SeedWork;
 
 namespace VoucherService.Domain.VoucherAggregate
 {
     public class Cost : Entity
     {
+        public DateTime? Created { get; private init; }
+        public string ItemDescription { get; private set; }
+        public decimal UnitAmount { get; private set; }
+        public decimal UnitQuantity { get; private set; }
+        public bool IsTax { get; private set; }
+
+        protected Cost() { }
+
+        public Cost(string itemDescription, decimal unitAmount, decimal unitQuantity, bool isTax)
+        {
+            Created = DateTime.UtcNow;
+            ItemDescription = itemDescription;
+            UnitAmount = unitAmount;
+            UnitQuantity = unitQuantity;
+            IsTax = isTax;
+        }
+
+        public decimal GetCostTotal () 
+        { 
+            return UnitAmount * UnitQuantity; 
+        }
     }
 }
