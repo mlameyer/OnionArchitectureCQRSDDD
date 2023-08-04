1d5567182bbe79d755630cae90bcc110003b713f
Matthew LaMeyer <7293501+mlameyer@users.noreply.github.com>
2023-08-04 13:32:40 -0500

    yay

--- VoucherService.Api/VoucherService.Application/testDomain.cs
+++ VoucherService.Api/VoucherService.Application/testDomain.cs
@@ -5,8 +5,18 @@ namespace VoucherService.Application
     public class testDomain
     {
         //Voucher voucher = new Voucher("1234");
-        //FlatCostAdjustment flatCostAdjustment = new FlatCostAdjustment(AdjustmentCode.Flat_Amount, AdjustmentType.CostAdjustment, 10);
+        FlatCostAdjustment flatCostAdjustment = new FlatCostAdjustment(AdjustmentCode.Flat_Amount, AdjustmentType.CostAdjustment, 10);
         Cost newCost = new Cost("cost", 100, 100, false);
+
+        public testDomain()
+        {
+                
+        }
+
+        public void test()
+        {
+            newCost.AddAdjustment(flatCostAdjustment);
+        }
 
     }
 }
